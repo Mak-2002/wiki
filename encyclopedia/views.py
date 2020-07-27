0886b2d85b7f3f4c4a37e397ab5c75a01823d581
Kusai Nasr <Kusai.Nasr.2002@users.noreply.github.com>
2020-07-27 20:12:31 +0300

    some changes (not potential)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
+class new_entry_form(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def new(request):
+    return render(request, "encyclopedia/new.html",{
+        "form" : new_entry_form()
+    })
