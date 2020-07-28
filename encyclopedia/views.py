a7928fa33272a7f667ce86760f4777427c042d59
Kusai Nasr <Kusai.Nasr.2002@users.noreply.github.com>
2020-07-28 19:45:04 +0300

    first successful commit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+import random
 
 from . import util
+
+class new_entry_form(forms.Form):
+    title = forms.CharField(min_length=1, label="")
+    content = forms.CharField(min_length=1, label="", widget = forms.Textarea)
 
 
 def index(request):
@@ -8,3 +15,24 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def new(request):
+    if request.method == "POST" :
+        form = new_entry_form(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            if util.get_entry(title) == None:
+                util.save_entry(title, content)
+                return HttpResponseRedirect(f"/wiki/{title}")
+            return render(request, "encyclopedia/new.html", {
+                "form" : new_entry_form(request.POST),
+                "err_found" : "Sorry, there's an existing page with this title"
+            })
+    return render(request, "encyclopedia/new.html", {
+        "form" : new_entry_form(),
+        "err_found" : ""
+    })
+
+def random_entry(request):
+    return HttpResponseRedirect(f"/wiki/{random.choice(util.list_entries()) }")
