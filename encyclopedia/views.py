8dac4bf2fdea0356e8deafd51ad46d60c57dd1de
Kusai Nasr <Kusai.Nasr.2002@users.noreply.github.com>
2020-07-28 14:07:04 +0300

    added new modifies

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
-def index(request):
+class new_entry_form(forms.Form):
+    title = forms.CharField(min_length=1)
+    content = forms.CharField(min_length=1, widget=forms.Textarea)
 
+def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def new(request):
-    return render(request, "encyclopedia/new.html")
+    if request.method == "POST":
+        form = new_entry_form(request.POST)
+        if util.get_entry(form.cleaned_data["title"]):
+            return render(request, "encyclopedia/new.html", {
+                "err_existed" : "Sorry, there's an existed page with the this title, please choose another one",
+                "form" : form
+            })
+    return render(request, "encyclopedia/new.html", {
+        "form" : new_entry_form(),
+        "err_existed" : ""
+    })
 
 def open_entry(request, entry):
     if util.get_entry(entry) == None :
